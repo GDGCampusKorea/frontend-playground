b8dcb08da1d0c2325972068b662da4e932a1bd22
Kevin Jin <tost8295@gmail.com>
2016-10-12 16:33:09 +0900

    theme switch, gitignore, readme

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Google Developers Group CAMPUS</h2>
+          <p>#frontend study'의 놀이터에 오신걸 환영합니다.</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
